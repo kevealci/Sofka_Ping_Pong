108e211176d8cc7835e49a9fe95543a9b602d7e7
kevealci <kevealci@espol.edu.ec>
2021-11-19 12:40:18 -0500

    Se agrego la funcionalidad de colisionar con las barras

--- Ball.mjs
+++ Ball.mjs
@@ -5,14 +5,35 @@ export class Ball {
   speedY;
   speedX;
   direction;
+  bounceAngle;
+  maxBounceAngle;
+  speed;
+  width;
 
-  constructor(x, y, radius) {
+  constructor(x, y, radius, width) {
     this.x = x;
     this.y = y;
     this.radius = radius;
     this.speedX = 3;
     this.speedY = 0;
     this.direction = 1;
+    this.bounceAngle = 0;
+    this.maxBounceAngle = Math.PI / 12;
+    this.speed = 3;
+    this.width = width;
+  }
+
+  collision(bar) {
+    let relativeIntersectY = bar.getY() + bar.getHeight() / 2 - this.y;
+    let normalizedIntersectY = relativeIntersectY / (bar.getHeight() / 2);
+
+    this.bounceAngle = normalizedIntersectY * this.maxBounceAngle;
+
+    this.speedY = this.speed * -Math.sin(this.bounceAngle);
+    this.speedX = this.speed * Math.cos(this.bounceAngle);
+
+    if (this.x > this.width / 2) this.direction = -1;
+    else this.direction = 1;
   }
 
   move() {
@@ -26,6 +47,14 @@ export class Ball {
 
   getY() {
     return this.y;
+  }
+
+  getWidth() {
+    return this.radius * 2;
+  }
+
+  getHeight() {
+    return this.radius * 2;
   }
 
   getRadius() {
