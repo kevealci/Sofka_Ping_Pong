fe5848c1c8380a35b601a9b5108bf5abce5f88f2
kevealci <kevealci@espol.edu.ec>
2021-11-19 09:07:44 -0500

    Se instanciaron los nuevos objetos y se probo el funcionamiento

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
-import { Board } from './Board';
-import { BoardView } from './BoardView';
+import { Bar } from './Bar.mjs';
+import { Board } from './Board.mjs';
+import { BoardView } from './BoardView.mjs';
 
 window.addEventListener('load', main);
 
 function main() {
   let board = new Board(800, 400);
+  let bar1 = new Bar(20, 100, 40, 100);
+  let bar2 = new Bar(80, 100, 40, 100);
+  board.addBar(bar1, bar2);
   let canvas = document.getElementById('canvas');
   let board_view = new BoardView(canvas, board);
+  board_view.graficar();
+  //console.log(board_view);
+  //console.log(board);
+  //console.log(bar1);
 }
