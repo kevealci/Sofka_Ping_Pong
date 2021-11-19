d848503d4f43946320a26cd4d68e0a47b996c8c4
kevealci <kevealci@espol.edu.ec>
2021-11-19 09:05:22 -0500

    Se agrego un metodo para agregar barras al board

---
+++ Board.mjs
@@ -0,0 +1,25 @@
+export class Board {
+  width;
+  height;
+  playing;
+  game_over;
+  bars;
+  ball;
+
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+    this.playing = false;
+    this.game_over = false;
+    this.bars = [];
+    this.ball = null;
+  }
+
+  getBars() {
+    return this.bars;
+  }
+
+  addBar(...bar) {
+    this.bars.push(bar);
+  }
+}
