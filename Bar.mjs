b72b612610a390298149b0c5d6e4070b15981364
kevealci <kevealci@espol.edu.ec>
2021-11-19 09:01:13 -0500

    Se agrego el modulo de Bar para agregar las barras del juego.

---
+++ Bar.mjs
@@ -0,0 +1,33 @@
+export class Bar {
+  x;
+  y;
+  width;
+  height;
+
+  constructor(x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+  }
+
+  getX() {
+    return this.x;
+  }
+
+  getY() {
+    return this.y;
+  }
+
+  getWidth() {
+    return this.width;
+  }
+
+  getHeight() {
+    return this.height;
+  }
+
+  down() {}
+
+  up() {}
+}
